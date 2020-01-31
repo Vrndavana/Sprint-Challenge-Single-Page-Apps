e64bed956a418f29fddc1cb26e7d95d6542d9be2
Vrndavana <Vrmdavama.th@gmail.com>
2020-01-31 10:55:40 -0800

    Sprint: Singe Page App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { Route } from "react-router-dom";
+import CharacterList from "./components/CharacterList"
+import WelcomePage from "./components/WelcomePage.js";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      
+      <Route exact path="/"> <WelcomePage/> </Route>
+      <Route path="/CharacterList"> <CharacterList/> </Route>
     </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,20 @@
 import React from "react";
 
+
 export default function CharacterCard() {
-  return <span>todo: character</span>;
+  return (
+    <div classNAme="character-card"> 
+    <Card>
+      <CardImg top-width="100" src= {props.image} alt="Card image"/>
+      <Cardbody>
+        <CardTitle>{props.name}</CardTitle>
+        <CardText>Status: {props.status}</CardText>
+        <CardText>Species: {props.species}</CardText>
+        <CardText>Gender: {props.gender}</CardText>
+      </Cardbody>
+    </Card>
+    </div>
+  
+);
+
 }
